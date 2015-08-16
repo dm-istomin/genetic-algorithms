b360b99a241405fa2c4a3023dc5bb88034b88598
dm-istomin <dmi.istomin@gmail.com>
2015-08-16 15:34:54 -0400

    implement fitness function, create canvases for other candidte solutions, performance improvements for fitness RGBA comparison

--- procedural_image_generation/app.js
+++ procedural_image_generation/app.js
@@ -33,9 +33,10 @@ var constants = {};
 
 constants.MUTATION_RATE          = 0.01;
 constants.CROSSOVER_RATE         = 0.7;
-constants.POPULATION_SIZE        = 100;
+constants.POPULATION_SIZE        = 20;
 constants.NUM_SHAPES_PER_IMAGE   = 50;
 constants.NUM_VERTICES_PER_SHAPE = 3;
+constants.SAMPLE_SIZE            = 50000;
 
 // generate random number given a max and min
 
@@ -76,29 +77,12 @@ function drawCanvas(args) {
       ctx.closePath(path);
     }
 
-    var generatedPixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    // var generatedPixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 
-    console.log('DATA URL, GENERATED CANVAS: ', canvas.toDataURL());
-    console.log('GENERATED LENGTH: ', canvas.toDataURL().length);
-    console.log('GENERATED PIXEL DATA: ', generatedPixelData);
-    console.log('GENERATED IMAGE DATA LENGTH: ', generatedPixelData.length);
-}
-
-function getFitnessScore(targetPixelData, generatedPixelData) {
-  var diffs = [];
-
-  for (var i = 0; i < generatedPixelData.length; i++) {
-    var fitness = 1 / Math.abs(targetPixelData[i] - generatedPixelData[i]);
-    fitness === 1 ? fitness = 0.99 : fitness;
-    fitness === Infinity ? fitness = 1.0 : fitness;
-
-    diffs.push(fitness);
-  }
-  var avgFitness = diffs.reduce(function(sum, val) {
-    return sum += val;
-  }) / generatedPixelData.length;
-
-  return avgFitness;
+    // console.log('DATA URL, GENERATED CANVAS: ', canvas.toDataURL());
+    // console.log('GENERATED LENGTH: ', canvas.toDataURL().length);
+    // console.log('GENERATED PIXEL DATA: ', generatedPixelData);
+    // console.log('GENERATED IMAGE DATA LENGTH: ', generatedPixelData.length);
 }
 
 function fileToCanvas(args) {
@@ -119,8 +103,10 @@ function fileToCanvas(args) {
   reader.readAsDataURL(file);
 }
 
-function SeedImage() {
+function SeedImage(canvas) {
   this.shapeData   = [];
+  this.canvas      = canvas;
+  this.ctx         = canvas.getContext('2d');
 
   for (var i = 0; i < constants.NUM_SHAPES_PER_IMAGE; i++) {
     this.shapeData.push({color: generateRandomColor(), vertices: []});
@@ -133,49 +119,90 @@ function SeedImage() {
   }
 }
 
-function Population() {
+function Population(container) {
   this.members   = [];
 
   for (var i = 0; i < constants.POPULATION_SIZE; i++) {
-    this.members.push(new SeedImage());
+    container.innerHTML += '<canvas height="500" width="500" class="generated-canvas" data-id="' + i + '"></canvas>';
+    this.members.push(new SeedImage(container.children[i]));
+
+    drawCanvas({
+      canvas: this.members[i].canvas,
+      ctx: this.members[i].ctx,
+      imageData: this.members[i]
+    })
   }
 }
 
-// function Population.prototype.
+Population.prototype.assignFitness = function(targetRGBA) {
+  var start = Date.now();
+
+  for (var i = 0; i < this.members.length; i++) {
+    var diffs          = [],
+        diffSum        = 0,
+        checkedIndices = {};
+
+    var generatedRGBA = this.members[i].ctx
+        .getImageData(0, 0, constants.X_LIMIT, constants.Y_LIMIT).data;
+
+    for (var j = 0; j < constants.SAMPLE_SIZE; j++) {
+      var randomIndex = randomInt(generatedRGBA.length);
+
+      if(randomIndex !== 0 && randomIndex % 4 == 0) { continue; } // ignore alpha values
+      if (checkedIndices[randomIndex] === true) { continue; }     // do not re-check values
+
+      var fitness = 1 / Math.abs(targetRGBA[randomIndex] - generatedRGBA[randomIndex]);
+      fitness === 1 ? fitness = 0.99 : fitness;       // normalize values
+      fitness === Infinity ? fitness = 1.0 : fitness; // handle division by 0
+
+      diffs.push(fitness);
+      diffSum += fitness;
+      checkedIndices[randomIndex] = true;
+    }
+    this.members[i].fitness = diffSum / diffs.length;
+  }
+
+  var end = Date.now();
+  console.log('RUNTIME FOR FITNESS FUNCTION: ', end - start, 'ms');
+};
+
+Population.prototype.rouletteWheelSelect = function() {
+
+};
+
+
 
 // Get image data out of a file uploaded by the user and draw it to canvas:
 
-document.addEventListener('DOMContentLoaded', function() {
-  var inputField      = document.querySelector('input'),
-      uploadForm      = document.querySelector('form'),
-      clearButton     = document.getElementById('clear-canvas'),
-      generatedCanvas = document.getElementById('generated-image'),
-      originCanvas    = document.getElementById('original-image');
+var inputField      = document.querySelector('input'),
+    uploadForm      = document.querySelector('form'),
+    clearButton     = document.getElementById('clear-canvas'),
+    generatedCanvas = document.getElementById('generated-image'),
+    originCanvas    = document.getElementById('original-image');
 
-  var ctxGenerated = generatedCanvas.getContext('2d'),
-      ctxOrigin    = originCanvas.getContext('2d');
+var ctxGenerated = generatedCanvas.getContext('2d'),
+    ctxOrigin    = originCanvas.getContext('2d');
 
-  constants.X_LIMIT = generatedCanvas.width;
-  constants.Y_LIMIT = generatedCanvas.height;
+constants.X_LIMIT = generatedCanvas.width;
+constants.Y_LIMIT = generatedCanvas.height;
 
-  clearButton.addEventListener('click', function() {
-    ctxGenerated.clearRect(0, 0, constants.X_LIMIT, constants.Y_LIMIT);
+clearButton.addEventListener('click', function() {
+  ctxGenerated.clearRect(0, 0, constants.X_LIMIT, constants.Y_LIMIT);
+});
+
+var population = new Population(document.getElementsByClassName('all-images')[0]);
+
+uploadForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if (!inputField.files.item(0)) { alert('No file submitted!'); return; }
+
+  fileToCanvas({
+    canvas: originCanvas,
+    ctx: ctxOrigin,
+    file: inputField.files.item(0)
   });
 
-  uploadForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    if (!inputField.files.item(0)) { alert('No file submitted!'); return; }
-
-    fileToCanvas({
-      canvas: originCanvas,
-      ctx: ctxOrigin,
-      file: inputField.files.item(0)
-    });
-
-    drawCanvas({
-      canvas: generatedCanvas,
-      ctx: ctxGenerated,
-      imageData: new SeedImage()
-    });
-  });
+  ctxGenerated.putImageData(population.members[4].ctx.getImageData(0, 0, 500, 500), 0, 0);
+  population.assignFitness(ctxOrigin.getImageData(0,0,500,500).data);
+  console.log('POPULATION: ', population);
 });
